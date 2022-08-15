98f983ac72274a62211ace08acc33945b0cb2def
Leandro Müller <leandromllr9@gmail.com>
2022-08-15 12:05:35 -0300

    Modificações finais

---
+++ Parada 28/script.js
@@ -0,0 +1,53 @@
+let slidePos = 1;
+let slidePosAut = 1;
+let habAut = 0;
+
+mostrarSlide(slidePos)
+
+function mostrarSlide(n){
+    let slides = document.getElementsByClassName("slide")
+
+    if(n>slides.length){
+        slidePos = 1;
+    }
+
+    if(n<=0){
+        slidePos = slides.length;
+    }
+
+    for(let i =0; i<slides.length;i++){
+        slides[i].style.display = 'none'
+    }
+
+    slides[slidePos-1].style.display = 'block'
+    
+}
+
+function passarSlide(n){
+    habAut = 1
+    mostrarSlide(slidePos+=n)
+}
+
+function passarSlideAut(){
+    let slides = document.getElementsByClassName("slide")
+
+    if(slidePosAut>slides.length){
+        slidePosAut = 1;
+    }
+
+    if(slidePosAut<=0){
+        slidePosAut = slides.length;
+    }
+
+    for(let i =0; i<slides.length;i++){
+        slides[i].style.display = 'none'
+    }
+
+    slides[slidePosAut-1].style.display = 'block'
+
+    slidePosAut+=1
+}
+
+if(habAut == 0){
+    setInterval(passarSlideAut,4500)
+}
