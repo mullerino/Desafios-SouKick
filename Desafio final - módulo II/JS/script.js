359788211b89b61d5e48081c2c32d23d0db6ca39
Leandro <leandromllr9@gmail.com>
2022-11-13 20:42:29 -0300

    Organiznado branch

--- Desafio final - módulo II/JS/script.js
+++ Desafio final - módulo II/JS/script.js
@@ -1,78 +1,6 @@
-let slidePos = 1;
-let slidePosAut = 1;
-let slideAut
-let slidePosSobre = 1
 const forms = document.getElementById('form')
 const subtitle = document.querySelector('h2')
 const title = document.querySelector('.line')
-
-//mostrarSlide(slidePos)
-//slideAut = setInterval(passarSlideAut,4500)
-//mostrarSlideSobre(slidePosSobre)
-/*function mostrarSlide(n){
-    let pontos = document.getElementsByClassName("ponto")
-    let slides = document.getElementsByClassName("slide")
-    if(n>slides.length){
-        slidePos = 1;
-    }
-    if(n<=0){
-        slidePos = slides.length;
-    }
-    for(let i =0; i<slides.length;i++){
-        slides[i].style.display = 'none'
-    }
-    for(let i =0; i<pontos.length;i++){
-        pontos[i].className = pontos[i].className.replace('active','')
-    }
-    pontos[slidePos-1].className += ' active'
-    //slides[slidePos-1].style.display = 'block'
-}
-function passarSlide(n){
-    clearInterval(slideAut)
-    mostrarSlide(slidePos+=n)
-    slideAut = setInterval(passarSlideAut,4500)
-}
-function passarSlidePonto(n){
-    clearInterval(slideAut)
-    mostrarSlide(slidePos=n)
-    slideAut = setInterval(passarSlideAut,4500)
-}
-function passarSlideAut(){
-    let pontos = document.getElementsByClassName("ponto")
-    let slides = document.getElementsByClassName("slide")
-    if(slidePosAut>slides.length){
-        slidePosAut = 1;
-    }
-    if(slidePosAut<=0){
-        slidePosAut = slides.length;
-    }
-    for(let i =0; i<slides.length;i++){
-        slides[i].style.display = 'none'
-    }
-    for(let i =0; i<pontos.length;i++){
-        pontos[i].className = pontos[i].className.replace('active','')
-    }
-    pontos[slidePosAut-1].className += ' active'
-    //slides[slidePosAut-1].style.display = 'block'
-    slidePosAut+=1
-}
-function mostrarSlideSobre(n){
-    let slidesSobre = document.getElementsByClassName('sobre-card');
-    if(n>slidesSobre.length){
-        slidePosSobre = 1;
-    }
-    if(n<=0){
-        slidePosSobre = slidesSobre.length
-    }
-    for(let i = 0; i<slidesSobre.length; i++){
-        slidesSobre[i].style.display = 'none'
-    }
-    slidesSobre[slidePosSobre-1].style.display = 'flex'
-}
-function passarSlideSobre(n){
-    mostrarSlideSobre(slidePosSobre+=n)
-}
-*/
 
 function confirmarEnvio(){
     let inputName = document.getElementById('nome')
@@ -187,24 +115,9 @@ roboCima = document.getElementsByClassName('robo-anima')
 moverDir = document.getElementsByClassName('move')
 moverEsq = document.getElementsByClassName('moveb')
 
-/*function playAnim(robo,tipo_anim){
-    if(loading()){
-        tipo_anim[0].style.animationPlayState = 'running'
-        robo[0].style.display = 'block'
-    }
-}
-function stopAnim(robo,tipo_anim){
-    tipo_anim[0].style.animationPlayState = 'paused'
-    robo[0].style.display = 'none'
-}
-playAnim(roboCima, moverEsq)
-*/
-
 var largura = window.innerWidth
 document.documentElement.clientWidth
 document.body.clientWidth;
-var test = ''
-let x = 0 
 
 function moverRobo(){
     let i = 0
